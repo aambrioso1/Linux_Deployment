5a91874be45e646f707bacd7e0197e0a6852852f
aambrioso <aambrioso1@gmail.com>
2020-12-29 22:15:11 -0500

    modify hello_javascript

--- hello_world_apps/hello_javascript.js
+++ hello_world_apps/hello_javascript.js
@@ -3,7 +3,8 @@
 
 const http = require('http');
 
-const hostname = 'nodeapp.alexambrioso.com';
+//const hostname = 'nodeapp.alexambrioso.com';
+const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
